5a7b0bc400cf0d02e9efb9bf0d8d2c2ca743cc4f
shivamsd2006 <shivamdabral18@gmail.com>
2025-09-16 23:13:24 +0530

    added analogies

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+function welcomePage() {
     let uploadbtn = document.getElementById('uploadbtn');
     if (uploadbtn) {
         uploadbtn.addEventListener('click', function () {
@@ -34,69 +34,9 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
     }
+}
 
-
-
-    const genBtn = document.getElementById('generate');
-    if (genBtn) {
-
-        genBtn.addEventListener('click', async () => {
-            try {
-                console.log("Clicked")
-                const prompt = localStorage.getItem('uploadedContent') || 'NO Content Uploaded';
-                console.log('PROMPT:------------------', prompt);
-                const ques = await CallAi('generate 5 curiosity questions from this text:' + prompt);
-                console.log("QUES------------------", ques);
-                if (ques) {
-                    alert("Generating Questions For You")
-                    const aiGeneratedQuestions = document.getElementById('aiGeneratedQuestions');
-                    const quesArray = ques.split('\n');
-                    for (q of quesArray) {
-                        const list = document.createElement('li');
-                        list.innerHTML = q.trim();
-                        aiGeneratedQuestions.appendChild(list);
-                    }
-
-                }
-            }
-            catch (error) {
-                alert('Error: ' + error);
-            }
-        })
-
-
-    }
-
-
-
-
-
-    async function CallAi(prompt) {
-        try {
-            const response = await fetch('/api/generate', {
-                method: 'POST',
-                headers: {
-                    'content-Type': 'application/json',
-                },
-                body: JSON.stringify({ prompt: prompt })
-
-            });
-
-            if (!response.ok) {
-                throw new Error("FAILED TO GET RESPONSE FROM SERVER");
-            }
-
-            const data = await response.json();
-            return data.feedback;
-        } catch (error) {
-            console.error('ERROR connecting to backend:', error);
-            alert('An error occurred: ' + error.message);
-            return null;
-        }
-    }
-
-
-
+function priming() {
 
     const questionForm = document.getElementById('questionForm');
     if (questionForm) {
@@ -124,11 +64,119 @@ document.addEventListener('DOMContentLoaded', () => {
 
         })
 
+    }
+    const genBtn = document.getElementById('generate');
+    if (genBtn) {
 
+        genBtn.addEventListener('click', async () => {
+            try {
+                console.log("Clicked")
+                const content = localStorage.getItem('uploadedContent') || 'NO Content Uploaded';
+                console.log('PROMPT:------------------', content);
+                const ques = await CallAi('generate 5 curiosity questions from this text:' + content);
+                console.log("QUES------------------", ques);
+                if (ques) {
+                    alert("Generating Questions For You")
+                    const aiGeneratedQuestions = document.getElementById('aiGeneratedQuestions');
+                    const quesArray = ques.split('\n');
+                    for (q of quesArray) {
+                        const list = document.createElement('li');
+                        list.innerHTML = q.trim();
+                        aiGeneratedQuestions.appendChild(list);
+                    }
 
-
+                }
+            }
+            catch (error) {
+                alert('Error: ' + error);
+            }
+        })
 
 
     }
+}
+
+
+
+
+
+async function CallAi(prompt) {
+    try {
+        const response = await fetch('/api/generate', {
+            method: 'POST',
+            headers: {
+                'content-Type': 'application/json',
+            },
+            body: JSON.stringify({ prompt: prompt })
+
+        });
+
+        if (!response.ok) {
+            throw new Error("FAILED TO GET RESPONSE FROM SERVER");
+        }
+
+        const data = await response.json();
+        return data.feedback;
+    } catch (error) {
+        console.error('ERROR connecting to backend:', error);
+        alert('An error occurred: ' + error.message);
+        return null;
+    }
+}
+
+
+
+
+
+
+function makeAnalogie() {
+    const userInput = document.getElementById('inputAnalogie');
+    const uploadBtn = document.getElementById('uploadBtn');
+    uploadBtn.addEventListener('click', async () => {
+        if (userInput.value.trim() != '') {
+            const analogie = userInput.value.trim();
+            const content = localStorage.getItem('uploadedContent');
+            const prompt = `As a critical thinker how this analogie :${analogie} makes a connection with this text: ${content}`;
+            const feedback = await CallAi(prompt);
+            if (feedback) {
+                alert('analogie uploaded');
+                const p = document.createElement('p');
+                p.innerHTML = feedback;
+                const feedbackBox = document.getElementById('feedbackBox');
+                feedbackBox.appendChild(p);
+            }
+        } else {
+            alert('Please type/paste your Anologie');
+        }
+    })
+    const generateBtn = document.getElementById('generateBtn');
+    generateBtn.addEventListener('click', async () => {
+        const content = localStorage.getItem('uploadedContent');
+        const prompt = `As a critical thinker make 2 analogies under 150 words for understanding this topic deeply: ${content} `;
+        const ans = await CallAi(prompt);
+        if (ans) {
+            alert('Creating a analogie for you');
+            const p = document.createElement('p');
+            p.innerHTML = ans;
+            const aiAnalogie = document.getElementById('aiAnalogie');
+            aiAnalogie.appendChild(p);
+        } else {
+            alert('Failed to create Analogie');
+        }
+
+    })
+
+}
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.getElementById('fileInput')) {
+        welcomePage();
+    }
+    else if (document.getElementById('questionForm')) {
+        priming();
+    }
+    else if (document.getElementById('inputAnalogie')) {
+        makeAnalogie();
+    }
+
 
 });
