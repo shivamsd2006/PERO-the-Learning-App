580be4087b875fb04ec5eedd0bc0749d4a807a2a
shivamsd2006 <shivamdabral18@gmail.com>
2025-10-03 11:24:32 +0530

    Integration of React

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Welcome from 'CurrentPage.jsx'
+import Priming from 'CurrentPage.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
