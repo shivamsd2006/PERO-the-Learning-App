243bb2505fb12ff91d525ea56219ffe04b194eee
shivamsd2006 <shivamdabral18@gmail.com>
2025-11-03 15:58:08 +0530

    added state vaiable

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,46 +6,46 @@ import SimplifyPage from './SimplifyPage'
 import GroupingPage from './GroupingPage'
 import RetrievalPage from './RetrievalPage'
 import OverlearningPage from './OverlearningPage'
+import { useState } from 'react'
 
-function App() {
+const [content, setContent] = useState('');
+const [page, setPage] = useState('');
 
-    const appState = {
-        uploadedContent: '',
-    };
+setContent(c){
+    content = c;
+}
 
-    function handleContentUpload() {
-        const textInput = document.getElementById('textInput');
-        const fileInput = document.getElementById('fileInput');
-        if (textInput.value.trim() != '') {
-            appState.uploadedContent = textInput.value.trim();
-            alert('Content Uploaded');
+function handleContentUpload() {
+    if (textInput.value.trim() != '') {
+        setContent(textInput.value.trim());
+        alert('Content Uploaded');
+    }
+    else if (fileInput.files.length > 0) {
+        const file = fileInput.files[0];
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const content = e.target.result;
+            setContent(content);
+            alert('file uploaded successfully');
         }
-        else if (fileInput.files.length > 0) {
-            const file = fileInput.files[0];
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                const content = e.target.result;
-                appState.uploadedContent = content;
-                alert('file uploaded successfully');
-            }
-            reader.onerror = () => {
-                alert('please upload a valid file');
-            }
-            if (file.type === 'text/plain') {
-                reader.readAsText(file);
-            } else {
-                alert("Unsupported File");
-            }
+        reader.onerror = () => {
+            alert('please upload a valid file');
         }
-        else if (textInput.value == '' && fileInput.files.length == 0) {
-            alert("Paste/type text or upload a file");
+        if (file.type === 'text/plain') {
+            reader.readAsText(file);
+        } else {
+            alert("Unsupported File");
         }
     }
-    return (
-        <>
-            <WelcomePage> onUpload = {handleContentUpload}</WelcomePage>
+    else if (textInput.value == '' && fileInput.files.length == 0) {
+        alert("Paste/type text or upload a file");
+    }
+}
+return (
+    <>
+        <WelcomePage> onUpload = {handleContentUpload}</WelcomePage>
 
-        </>
-    );
+    </>
+);
 }
 export default App
