4bf713733d9ae006a2b57a8da1d2e2cc95cfe71c
Shivamsd2006 <gautamdabral1999@gmail.com>
2025-12-06 10:43:44 +0530

    yaml created

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/PERO-the-Learning-App/',
   server: {
     proxy: {
       '/api': {
